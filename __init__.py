02d2fa21875ef0dc5ab91638d09d470c058fe5bd
Artur Artemij Monsch <60860160+a-monsch@users.noreply.github.com>
2026-01-20 12:24:01 +0100

    Adding importable constants, functions and classes

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .Logging import (
+    CONSOLE,
+    LOG_LEVEL,
+    LogContext,
+    LogDecorator,
+    setup_logging,
+)
+
+__all__ = ["setup_logging", "LogContext", "LogDecorator", "CONSOLE", "LOG_LEVEL"]
